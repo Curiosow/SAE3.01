e30ab878c5b8cfb1c6220bb41878caf8c6d7829b
Oscar <teampandapro45@gmail.com>
2024-09-19 15:52:56 +0200

    update : connexion button color to actual A.D.

--- Login.php
+++ Login.php
@@ -73,7 +73,7 @@ if(isset($_POST['groupe'])) {
             </div>
 
             <div>
-                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Connexion</button>
+                <button type="submit" class="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">Connexion</button>
             </div>
         </form>
     </div>
