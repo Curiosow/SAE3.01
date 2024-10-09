0039ef322fd22169c8fca2408b29fd6fd7965d98
Oscar <teampandapro45@gmail.com>
2024-10-09 10:26:44 +0200

    update : add short function in EnseignementManager.php

--- managers/EnseignementManager.php
+++ managers/EnseignementManager.php
@@ -15,3 +15,19 @@ function getEnseignementFullName($id) {
 
     return pg_fetch_assoc($result)['long'];
 }
+
+function getEnseignementShortName($id) {
+    if(!isset($id)) {
+        return '';
+    }
+
+    $preparedStatement = "SELECT court FROM enseignement WHERE code=$1";
+    $connexion = Database::getInstance()->getConnection();
+    if(!$connexion) {
+        die('La communcation à la base de données a echouée : ' . pg_last_error());
+    }
+
+    $result = pg_query_params($connexion, $preparedStatement, array($id));
+
+    return pg_fetch_assoc($result)['court'];
+}
