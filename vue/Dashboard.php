f9ea68f8c5d94daef0d4d9006e35d4cd3eab50e1
CorentinChr <corentincharpiot@gmail.com>
2024-10-09 10:08:49 +0200

    Update : start of mvc implementation
    Signed-off-by: CorentinChr <corentincharpiot@gmail.com>

--- vue/Dashboard.php
+++ vue/Dashboard.php
@@ -2,7 +2,6 @@
 include "../controleur/Controleur.php";
 
 session_start();
-
 global $controleur;
 $controleur = new Controleur();
 
@@ -93,66 +92,7 @@ function generateCalendar() {
     }
 }
 
-function generateDays() {
-    global $week;
 
-    $weekDates = getWeekDates($week);
-    foreach ($weekDates as $weekDate) {
-        $courses = $controleur.controlGetDay($weekDate, $weekDate->format('d'), (int) getSemestre((int) $_SESSION['promotion'], $weekDate), $_SESSION['groupe'], (int) $_SESSION['sousgroupe'], $_SESSION['formation']);
-        foreach ($courses as $course) {
-            $horraire = new DateTime($course->getHoraire(), new DateTimeZone('Europe/Paris'));
-            $dispHoraire = $horraire->format("N");
-            $dispGridRow = getGridRow($horraire);
-
-            $duree = new DateTime($course->getDuration(), new DateTimeZone('Europe/Paris'));
-            $dispSpan = getSpan($duree);
-
-            $color = 'red';
-            switch ($course->getTypeseance()) {
-                case 'CM':
-                    $color = 'purple';
-                    break;
-
-                case "TD":
-                    $color = 'blue';
-                    break;
-
-                case "TP":
-                    $color = 'green';
-                    break;
-
-                case "DS":
-                    $color = 'orange';
-                    break;
-
-                case "PRJ":
-                    $color = 'yellow';
-                    break;
-            }
-
-            $dispHour = (int)$horraire->format("H");
-            //$dispHour = $dispHour + 1;
-            if($dispHour < 10 && $dispHour > 1)
-                $dispHour = '0' . $dispHour;
-
-            $dispMinute = $horraire->format("i") . '';
-            if($horraire->format("i") < 10 && $horraire->format("i") > 1)
-                $dispMinute = '0' . $dispMinute;
-
-            // type="submit" name="viewCourse" value="' . $ser = serialize($course); echo $ser . '"
-            echo '<li class="relative mt-px flex sm:col-start-' . $dispHoraire . '" style="grid-row: ' . $dispGridRow . ' / span ' . $dispSpan . '">
-                <a class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-'. $color . '-50 p-2 text-xs leading-5 hover:bg-' . $color . '-100">
-                <form>
-                <button">
-                    <p class="text-'. $color . '-500 group-hover:text-'. $color . '-700"><time>'. $dispHour . ':' . $dispMinute . ' - ' . ($course->getSalle() == '' ? 'Pas de salle' : ($course->getSalle() == '200' ? 'Amphi.' : 'Salle ' . $course->getSalle())) . '</time></p>
-                    <p class="order-1 font-semibold text-'. $color . '-700">' . $course->getTypeseance() . ' - ' . removeAfterTiret($course->getCode()) . '</p>
-                    <p class="order-1 font-semibold text-'. $color . '-700">' . getCollegueFullName($course->getCollegue()) . '</p>
-                </button>
-              </form>
-              </a>';
-        }
-    }
-}
 
 function removeAfterTiret($string) {
     $parts = explode(' - ', $string);
@@ -443,7 +383,7 @@ function getWeekDay($firstDay) {
                                 <!-- Chaque incrément de cet argument augmente de 30 minutes le début, par exemple 8h : 2; 9h : 4 etc-->
                                 <!-- col-start correspond au jour de la semaine-->
                                 <!-- span correspond à la durée du cours, plus précisemment au nombre de demi heures, par exemple 1h30 : 3-->
-                                <?php generateDays(); ?>
+                                <?php $controleur->generateDays($week); ?>
                             </ol>
                         </div>
                     </div>
