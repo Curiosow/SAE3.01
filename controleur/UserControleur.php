0cb7fe88eba0a855a31406dcc6bade5468153f03
Oscar <teampandapro45@gmail.com>
2024-12-17 08:12:03 +0100

    update : fix database file access

--- controleur/UserControleur.php
+++ controleur/UserControleur.php
@@ -1,4 +1,5 @@
 <?php
+include_once "../modele/Database.php";
 
 class UserControleur
 {
--- controleur/UtilsControleur.php
+++ controleur/UtilsControleur.php
@@ -1,5 +1,4 @@
 <?php
-include_once("../modele/managers/ScheduleManager.php");
 
 function disconnect() {
     session_destroy();
