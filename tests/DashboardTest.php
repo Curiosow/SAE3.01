0b862502eba0c5a9e89fd4f8971d25d9500e4720
Maxime <maxime.bouju@gmail.com>
2025-01-15 13:07:23 +0100

    feat: implementation of PHPUnit

--- tests/DashboardTest.php
+++ tests/DashboardTest.php
@@ -3,12 +3,5 @@
 use PHPUnit\Framework\TestCase;
 class DashboardTest extends TestCase
 {
-    public function testGetDataReturnsNonEmptyArray(): void
-    {
-        $dashboard = new Dashboard();
-        $data = $dashboard->getData();
 
-        $this->assertIsArray($data);
-        $this->assertNotEmpty($data);
-    }
 }
